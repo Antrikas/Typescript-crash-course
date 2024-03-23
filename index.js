6a3ccdc06bee33a71fed96c80f462b3ed3568da7
Antrikas <andreas.antoniades17@gmail.com>
2024-03-23 12:14:51 +0200

    typescript-crash-course

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+//Basic Types
+let age = 25;
+let hobby = "soccer";
+let isAdult = true;
+let fruit = "apple";
+//Arrays
+let fruits = ["Apple", "Orange"];
+let arr = [22, "a", true];
+let numArr = [[1], [2]];
+//Tuples
+let employee = [1, "Jack"];
+let employees = [
+    [2, "Sarah"],
+    [3, "Jack"]
+];
+//Objects
+let car = {
+    brand: "Lexus",
+    year: 2020
+};
+let car1 = {
+    brand: "Lexus",
+    year: 2020
+};
+let newCar = {
+    brand: "Lexus",
+    year: 2020
+};
+//Union
+let x = 22;
+let jackOccupation = "student";
+//Type Assertion
+let y = "a";
+y = 22;
+//Functions
+function add(x, y) {
+    return x + y;
+}
+function sayHi() {
+    console.log("Hi");
+}
+//Generics
+function copyArr(arr) {
+    return [...arr];
+}
+let numCopy = copyArr([1, 2, 3]);
+let strCopy = copyArr(["1", "2", "3"]);
+//Enums
+var EyeColor;
+(function (EyeColor) {
+    EyeColor["brown"] = "Common ey color";
+    EyeColor["blue"] = "Rare eye color";
+    EyeColor["green"] = "Very rare eye color";
+})(EyeColor || (EyeColor = {}));
+function displayStudent(student) {
+    const info = student.name + "\n" + student.age + "\n" + student.eyeColor;
+    document.body.innerText = info;
+}
+displayStudent({
+    name: "Jack",
+    age: 20,
+    eyeColor: EyeColor.green
+});
---
+++ index.ts
@@ -0,0 +1,100 @@
+//Basic Types
+let age: number = 25
+let hobby: string = "soccer"
+let isAdult: boolean = true
+let fruit: any = "apple"
+
+//Arrays
+let fruits: string [] = ["Apple", "Orange"]
+let arr: any[] = [22, "a", true]
+let numArr: number[] [] =[[1],[2]]
+
+//Tuples
+let employee: [number, string] = [1, "Jack"]
+let employees: [number, string] [] =[
+    [2, "Sarah"],
+    [3, "Jack"]
+]
+
+//Objects
+let car: {
+    brand: string, 
+    year: number
+}= {
+    brand: "Lexus",
+    year: 2020
+}
+
+// Custom types
+type Car = {
+    brand: string,
+    year: number
+}
+
+let car1: Car ={
+    brand: "Lexus",
+    year: 2020
+}
+
+//interface
+interface Car1 {
+    brand: string,
+    year: number
+}
+
+let newCar: Car1 = {
+    brand: "Lexus",
+    year: 2020
+}
+
+//Union
+let x: string | number = 22
+
+type occupation = "employed" | "student"
+let jackOccupation: occupation = "student"
+
+//Type Assertion
+let y: any ="a"
+y = <number> 22
+
+//Functions
+function add(x: number, y: number): number {
+    return x + y
+}
+
+function sayHi(): void {
+    console.log("Hi")
+}
+
+//Generics
+function copyArr<T>(arr: T []): T[]{
+    return [...arr]
+}
+
+let numCopy = copyArr<number>([1, 2, 3])
+let strCopy = copyArr<string>(["1", "2", "3"])
+
+
+//Enums
+enum EyeColor {
+    brown = "Common ey color",
+    blue = "Rare eye color",
+    green ="Very rare eye color"
+}
+
+interface Student {
+    name: string, 
+    age?: number,
+    eyeColor: EyeColor
+}
+
+function displayStudent(student: Student): void {
+    const info: string = student.name + "\n" +student.age + "\n" +student.eyeColor
+    document.body.innerText = info
+}
+
+displayStudent({
+    name: "Jack", 
+    age: 20, 
+    eyeColor: EyeColor.green
+})
